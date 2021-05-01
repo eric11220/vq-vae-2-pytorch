edddc7f963c83dbf3cf677159a13d25e83a1cd27
eric11220 <b99902080@ntu.edu.tw>
2021-05-01 10:06:36 -0500

    dataset and offset encoder

---
+++ offset_dataset.py
@@ -0,0 +1,62 @@
+import numpy as np
+import os
+import random
+import torch
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms, utils
+from PIL import Image
+
+class OffsetDataset(Dataset):
+    def __init__(self, frame_dir, offset=5, transform=None):
+        self.transform = transform
+
+        self.path_pairs = []
+        for vid in os.listdir(frame_dir):
+            vid_dir = os.path.join(frame_dir, vid)
+            for frame in sorted(os.listdir(vid_dir)):
+                frame_path = os.path.join(vid_dir, frame)
+                frame_no, ext = os.path.splitext(frame)
+                frame_no = int(frame_no)
+                next_frame = frame_no + offset
+                next_frame_path = os.path.join(vid_dir, f'{next_frame:04d}{ext}')
+                if os.path.isfile(next_frame_path):
+                    self.path_pairs.append((frame_path, next_frame_path))
+
+    def __len__(self):
+        return len(self.path_pairs)
+
+    def __getitem__(self, index):
+        frame_path, next_frame_path = self.path_pairs[index]
+        frame = Image.open(frame_path)
+        next_frame = Image.open(next_frame_path)
+
+        if self.transform is not None:
+            seed = np.random.randint(2147483647)
+            random.seed(seed); torch.manual_seed(seed)
+            frame_t = self.transform(frame)
+
+            seed = np.random.randint(2147483647)
+            random.seed(seed); torch.manual_seed(seed)
+            next_frame_t = self.transform(next_frame)
+
+        return frame_t, next_frame_t
+
+
+if __name__ == '__main__':
+    frame_dir = 'datasets/adobe_240fps/frames/train'
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(256),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+        ]
+    )
+    dataset = OffsetDataset(frame_dir, transform=transform)
+    loader = DataLoader(dataset, batch_size=16, num_workers=4)
+
+    # for frame, next_frame in loader:
+    #     print(frame.shape, next_frame.shape)
+    #     input("")
---
+++ offset_encoder.py
@@ -0,0 +1,91 @@
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from offset_dataset import OffsetDataset
+from vqvae import VQVAE
+
+class OffsetNetwork(VQVAE):
+    def __init__(self, vqvae, in_channel=6, channel=128, n_res_block=2, n_res_channel=32, embed_dim=64, n_embed=512, decay=0.99):
+
+        super(OffsetNetwork, self).__init__(
+            in_channel=in_channel,
+            channel=channel,
+            n_res_block=n_res_block,
+            n_res_channel=n_res_channel,
+            embed_dim=embed_dim,
+            n_embed=n_embed,
+            decay=decay
+        )
+
+        # Fix pre-trained VQVAE
+        self.vqvae = vqvae if vqvae is not None else VQVAE()
+        for params in self.vqvae.parameters():
+            params.requires_grad = False
+
+    def forward(self, frames, next_frames, offset_only=False):
+        inputs = torch.cat((frames, next_frames), dim=1)
+        offset_quant_t, offset_quant_b, diff, _, _ = self.encode(inputs)
+
+        quant_t = offset_quant_t
+        quant_b = offset_quant_b
+        if not offset_only:
+            frame1_quant_t, frame1_quant_b, _, _, _ = self.vqvae.encode(frames)
+            quant_t += frame1_quant_t
+            quant_b += frame1_quant_b
+
+        dec = self.vqvae.decode(quant_t, quant_b)
+        return dec, diff
+
+    def forward_offset(self, frames, next_frames, only_offset=False):
+        inputs = torch.cat((frames, next_frames), dim=1)
+        offset_quant_t, offset_quant_b, diff, _, _ = self.encode(inputs)
+        # frame1_quant_t, frame1_quant_b, _, _, _ = self.vqvae.encode(frames)
+        
+        quant_t = offset_quant_t
+        quant_b = offset_quant_b
+
+        dec = self.vqvae.decode(quant_t, quant_b)
+        return dec, diff
+
+
+if __name__ == '__main__':
+    frame_dir = 'datasets/adobe_240fps/frames/train'
+    ckpt = 'checkpoint/vqvae_175.pt'
+    device = 'cuda'
+
+    # Load pre-trained VQVAE
+    vqvae = VQVAE().to(device)
+    try:
+        vqvae.load_state_dict(torch.load(ckpt))
+    except:
+        print("Seems the checkpoint was trained with data parallel, try loading it that way")
+        weights = torch.load(ckpt)
+        renamed_weights = {}
+        for key, value in weights.items():
+            renamed_weights[key.replace('module.', '')] = value
+        weights = renamed_weights
+        vqvae.load_state_dict(weights)
+    vqvae.eval()
+
+    # Init offset encoder
+    model = OffsetNetwork(vqvae).to(device)
+
+    # Load Offset Dataset
+    transform = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+        ]
+    )
+    dataset = OffsetDataset(frame_dir, transform=transform)
+    loader = DataLoader(dataset, batch_size=16, num_workers=4)
+
+    for frames, next_frames in loader:
+        frames, next_frames = frames.to(device), next_frames.to(device)
+        out, latent_loss = model(frames, next_frames)
+        print(out.shape, latent_loss)
+        input("")
